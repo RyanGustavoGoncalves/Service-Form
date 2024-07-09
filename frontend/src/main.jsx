dd3d1ef44210b3debb15e0c87967ad06282229f9
RyanGustavoGoncalves <ryan_gustavo09@outlook.com>
2024-07-09 13:05:13 -0300

    chore: Add NotFoundPageError component for handling 404 errors

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,28 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { ThemeProvider } from './components/theme-provider.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { HomePage } from './pages/Home.jsx'
+import NotFoundPageError from './pages/components/Exceptions/NotFoundPageError.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <NotFoundPageError />,
+    children: [
+      {
+        path: "Home",
+        element: <HomePage />,
+      },
+    ],
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <App />
+      <RouterProvider router={router} />
     </ThemeProvider>
   </React.StrictMode>,
 )
